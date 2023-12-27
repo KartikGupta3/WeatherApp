3cc07d9cb45281ee3b31b4d09bbc23046bd22634
Kartik Gupta <kkartikgupta30@gmail.com>
2023-12-27 17:23:54 +0530

    added weather images

--- Components/Home.tsx
+++ Components/Home.tsx
@@ -1,17 +1,57 @@
 /* eslint-disable prettier/prettier */
-import React from 'react';
-import {Dimensions, Text, View} from 'react-native';
+/* eslint-disable react-native/no-inline-styles */
+import React, {useState} from 'react';
+import {Dimensions, Image, TextInput, View} from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
-const {height} = Dimensions.get('screen');
+const {height, width} = Dimensions.get('screen');
 const Home = () => {
+  const [weather] = useState<
+    'sunny' | 'cloudy' | 'partlycloudy' | 'rainy' | 'sunnyrainy'
+  >('rainy');
+
+  const weatherImages = {
+    sunny: require('./assets/sun.png'),
+    cloudy: require('./assets/cloudy.png'),
+    partlycloudy: require('./assets/PartlyCloudy.png'),
+    rainy: require('./assets/rain.png'),
+    sunnyrainy: require('./assets/SunnyRainy.png'),
+  };
   return (
     <LinearGradient
       colors={['#47BFDF', '#4A91FF']}
       start={{x: 1, y: 0}}
       end={{x: 0, y: 1}}
       style={{height: height}}>
-      <View>
-        <Text>Hello</Text>
+      <View
+        style={{
+          flex: 1,
+          flexDirection: 'column',
+          padding: 19,
+          rowGap: height * 0.12,
+        }}>
+        <View>
+          <TextInput
+            placeholder="Search"
+            placeholderTextColor="gray"
+            style={{
+              backgroundColor: 'white',
+              color: 'black',
+              width: width * 0.9,
+              borderRadius: 100,
+              paddingLeft: 20,
+            }}
+          />
+        </View>
+        <View
+          style={{
+            flex: 1,
+            alignItems: 'center',
+          }}>
+          <Image
+            source={weatherImages[weather]}
+            style={{width: width * 0.4, height: height * 0.2}}
+          />
+        </View>
       </View>
     </LinearGradient>
   );
