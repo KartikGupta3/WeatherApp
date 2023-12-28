8bdad4cede3aa06305784cba21b94903c1ea8133
Kartik Gupta <kkartikgupta30@gmail.com>
2023-12-28 16:24:31 +0530

    more images added and completed ui till forecast

--- Components/Home.tsx
+++ Components/Home.tsx
@@ -1,20 +1,21 @@
 /* eslint-disable prettier/prettier */
 /* eslint-disable react-native/no-inline-styles */
 import React, {useState} from 'react';
-import {Dimensions, Image, TextInput, View} from 'react-native';
+import {Dimensions, Image, Text, TextInput, View} from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 const {height, width} = Dimensions.get('screen');
 const Home = () => {
   const [weather] = useState<
-    'sunny' | 'cloudy' | 'partlycloudy' | 'rainy' | 'sunnyrainy'
-  >('rainy');
+    'sunny' | 'cloudy' | 'partlycloudy' | 'rainy' | 'sunnyrainy' | 'mist'
+  >('sunny');
 
   const weatherImages = {
     sunny: require('./assets/sun.png'),
-    cloudy: require('./assets/cloudy.png'),
+    cloudy: require('./assets/cloud.png'),
     partlycloudy: require('./assets/PartlyCloudy.png'),
-    rainy: require('./assets/rain.png'),
+    rainy: require('./assets/heavyrain.png'),
     sunnyrainy: require('./assets/SunnyRainy.png'),
+    mist: require('./assets/mist.png'),
   };
   return (
     <LinearGradient
@@ -27,7 +28,7 @@ const Home = () => {
           flex: 1,
           flexDirection: 'column',
           padding: 19,
-          rowGap: height * 0.12,
+          rowGap: height * 0.06,
         }}>
         <View>
           <TextInput
@@ -46,11 +47,46 @@ const Home = () => {
           style={{
             flex: 1,
             alignItems: 'center',
+            rowGap: 15,
           }}>
+          <View>
+            <Text style={{color: 'white', fontSize: 27}}>Delhi, India</Text>
+          </View>
           <Image
             source={weatherImages[weather]}
-            style={{width: width * 0.4, height: height * 0.2}}
+            style={{width: width * 0.5, height: height * 0.22}}
           />
+          <View>
+            <Text style={{color: 'white', fontSize: 48, paddingLeft: 16}}>
+              11{'\u00b0'}
+            </Text>
+          </View>
+          <View>
+            <Text style={{color: 'white', fontSize: 21}}>Sunny</Text>
+          </View>
+          <View style={{flex: 1, flexDirection: 'row', columnGap:40,padding:10}}>
+            <View style={{flex: 1, flexDirection: 'row', columnGap: 10}}>
+              <Image
+                source={require('./assets/wind.png')}
+                style={{width: 15, height: 15,marginTop:5}}
+              />
+              <Text style={{color: 'white', fontSize: 16}}>22km</Text>
+            </View>
+            <View style={{flex: 1, flexDirection: 'row', columnGap: 10}}>
+              <Image
+                source={require('./assets/drop.png')}
+                style={{width: 15, height: 15,marginTop:5}}
+              />
+              <Text style={{color: 'white', fontSize: 16}}>22%</Text>
+            </View>
+            <View style={{flex: 1, flexDirection: 'row', columnGap: 10}}>
+              <Image
+                source={require('./assets/sun2.png')}
+                style={{width: 15, height: 15,marginTop:5}}
+              />
+              <Text style={{color: 'white', fontSize: 16}}>6:00 am</Text>
+            </View>
+          </View>
         </View>
       </View>
     </LinearGradient>
