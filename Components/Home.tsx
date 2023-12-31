7b62a31cafd7d3a1c918487f52cbedf7cb32ce88
Kartik Gupta <kkartikgupta30@gmail.com>
2023-12-31 23:15:58 +0530

    debouncing search bar

--- Components/Home.tsx
+++ Components/Home.tsx
@@ -1,21 +1,32 @@
 /* eslint-disable prettier/prettier */
 /* eslint-disable react-native/no-inline-styles */
-import React, {useState} from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, {useCallback, useState} from 'react';
 import {
   Dimensions,
   Image,
   ScrollView,
   Text,
   TextInput,
+  TouchableOpacity,
   View,
 } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
+import axios from 'axios';
+import debounce from 'lodash/debounce';
 const {height, width} = Dimensions.get('screen');
+interface Location {
+  name: string;
+  country: string;
+}
+
 const Home = () => {
   const [weather] = useState<
     'sunny' | 'cloudy' | 'partlycloudy' | 'rainy' | 'sunnyrainy' | 'mist'
   >('sunny');
-
+  const [location, setLocations] = useState<Location[]>([]);
+  const [weatherforecast, setWeatherForecast] = useState({});
+  const apiKey = '9a98fd6da76e438b99943755232912';
   const weatherImages = {
     sunny: require('./assets/sun.png'),
     cloudy: require('./assets/cloud.png'),
@@ -24,6 +35,38 @@ const Home = () => {
     sunnyrainy: require('./assets/SunnyRainy.png'),
     mist: require('./assets/mist.png'),
   };
+  const forecast = useCallback(async (loc1: String) => {
+    const options = {
+      method: 'GET',
+      url: `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${loc1}&days=3`,
+    };
+
+    try {
+      const response = await axios.request(options);
+      setWeatherForecast(response.data);
+    } catch (error) {
+      console.log('error: ', error);
+      return {};
+    }
+  }, []);
+  const locations = async (loc: String) => {
+    const options = {
+      method: 'GET',
+      url: `https://api.weatherapi.com/v1/search.json?key=${apiKey}&q=${loc}`,
+    };
+
+    try {
+      const response = await axios.request(options);
+      setLocations(response.data);
+    } catch (error) {
+      console.log('error: ', error);
+      return {};
+    }
+  };
+
+  const handleSeachdebounce = useCallback(debounce(locations, 300), [
+    locations,
+  ]);
   return (
     <LinearGradient
       colors={['#47BFDF', '#4A91FF']}
@@ -38,6 +81,7 @@ const Home = () => {
           <TextInput
             placeholder="Search"
             placeholderTextColor="gray"
+            onChangeText={handleSeachdebounce}
             style={{
               backgroundColor: 'white',
               color: 'black',
@@ -46,6 +90,30 @@ const Home = () => {
               paddingLeft: 20,
             }}
           />
+          {location.length > 0 && (
+            <View style={{paddingLeft: 20, paddingRight: 20}}>
+              <View
+                style={{
+                  backgroundColor: 'white',
+                  paddingLeft: 10,
+                  borderBottomLeftRadius: 20,
+                  borderBottomRightRadius: 20,
+                }}>
+                {location.map((loc, index) => (
+                  <TouchableOpacity
+                    key={index}
+                    onPress={() => forecast(loc?.name)}
+                    style={{
+                      paddingVertical: 10,
+                    }}>
+                    <Text style={{color: 'black'}}>
+                      {loc?.name}, {loc?.country}
+                    </Text>
+                  </TouchableOpacity>
+                ))}
+              </View>
+            </View>
+          )}
         </View>
         <View style={{alignItems: 'center', padding: 19}}>
           <View>
@@ -133,8 +201,60 @@ const Home = () => {
             <View
               style={{
                 flexDirection: 'row',
-                columnGap: 5,
+                columnGap: 10,
               }}>
+              <View
+                style={{
+                  flexDirection: 'row',
+                  alignItems: 'center',
+                }}>
+                <View
+                  style={{
+                    flex: 1,
+                    flexDirection: 'column',
+                    backgroundColor: 'rgba(255, 255, 255, 0.25)',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    width: 100,
+                    height: 100,
+                    borderRadius: 20,
+                  }}>
+                  <Image
+                    source={require('./assets/sun.png')}
+                    style={{width: 30, height: 30}}
+                  />
+                  <Text style={{color: 'white', fontSize: 12}}>Friday</Text>
+                  <Text style={{color: 'white', fontSize: 24, paddingLeft: 5}}>
+                    11{'\u00b0'}
+                  </Text>
+                </View>
+              </View>
+              <View
+                style={{
+                  flexDirection: 'row',
+                  alignItems: 'center',
+                }}>
+                <View
+                  style={{
+                    flex: 1,
+                    flexDirection: 'column',
+                    backgroundColor: 'rgba(255, 255, 255, 0.25)',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    width: 100,
+                    height: 100,
+                    borderRadius: 20,
+                  }}>
+                  <Image
+                    source={require('./assets/sun.png')}
+                    style={{width: 30, height: 30}}
+                  />
+                  <Text style={{color: 'white', fontSize: 12}}>Friday</Text>
+                  <Text style={{color: 'white', fontSize: 24, paddingLeft: 5}}>
+                    11{'\u00b0'}
+                  </Text>
+                </View>
+              </View>
               <View
                 style={{
                   flexDirection: 'row',
